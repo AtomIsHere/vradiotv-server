35eaad8c42897bc3cf4a595e9d922d90b53e47f3
AtomIsHere <archietomoconnor@gmail.com>
2022-08-05 09:19:04 +1000

    Initial Commit

---
+++ src/main/java/tv/vradio/vradiotvserver/VradiotvServerApplication.java
@@ -0,0 +1,13 @@
+package tv.vradio.vradiotvserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VradiotvServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VradiotvServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tv/vradio/vradiotvserver/VradiotvServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tv.vradio.vradiotvserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VradiotvServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
