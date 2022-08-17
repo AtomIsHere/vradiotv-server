6ef2050692e7e5f208337736915cd44a2ccb5f9b
AtomIsHere <archietomoconnor@gmail.com>
2022-08-17 10:15:37 +1000

    Enable RabbitMQ

--- src/main/java/tv/vradio/vradiotvserver/VRadioTVServerApplication.java
+++ src/main/java/tv/vradio/vradiotvserver/VRadioTVServerApplication.java
@@ -1,8 +1,10 @@
 package tv.vradio.vradiotvserver;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication
 public class VRadioTVServerApplication {
     public static void main(String[] args) {
