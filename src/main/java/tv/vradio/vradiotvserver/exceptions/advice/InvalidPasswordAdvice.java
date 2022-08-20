fa23f6f36854522b0d0664375d09d8a0c07192f6
AtomIsHere <archietomoconnor@gmail.com>
2022-08-20 14:33:34 +1000

    Fix advice for invalid password

--- src/main/java/tv/vradio/vradiotvserver/exceptions/advice/InvalidPasswordAdvice.java
+++ src/main/java/tv/vradio/vradiotvserver/exceptions/advice/InvalidPasswordAdvice.java
@@ -5,13 +5,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import tv.vradio.vradiotvserver.exceptions.AccountNotFoundException;
 import tv.vradio.vradiotvserver.exceptions.InvalidPasswordException;
 
 @ControllerAdvice
 public class InvalidPasswordAdvice {
     @ResponseBody
-    @ExceptionHandler(AccountNotFoundException.class)
+    @ExceptionHandler(InvalidPasswordException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public String invalidPasswordHandler(InvalidPasswordException ex) {
         return ex.getMessage();
