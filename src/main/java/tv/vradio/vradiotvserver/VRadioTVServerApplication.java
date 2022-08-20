a777053d4d1ac0c92019233efb4c9d3d8df1a42a
AtomIsHere <archietomoconnor@gmail.com>
2022-08-20 14:36:48 +1000

    Remove some security features

--- src/main/java/tv/vradio/vradiotvserver/VRadioTVServerApplication.java
+++ src/main/java/tv/vradio/vradiotvserver/VRadioTVServerApplication.java
@@ -1,13 +1,25 @@
 package tv.vradio.vradiotvserver;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableRabbit
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class VRadioTVServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(VRadioTVServerApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*");
+            }
+        };
     }
 }
