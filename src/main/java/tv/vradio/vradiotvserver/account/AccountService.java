7958c8c7bcfd56a31b5daabef21126c289a78613
AtomIsHere <archietomoconnor@gmail.com>
2022-08-27 16:55:32 +1000

    Remove redundant classes

--- src/main/java/tv/vradio/vradiotvserver/account/AccountService.java
+++
@@ -1,7 +0,0 @@
-package tv.vradio.vradiotvserver.account;
-
-public interface AccountService {
-    String generateToken(Account account);
-    boolean confirmToken(Account account, String token);
-    void deauth(Account account);
-}
