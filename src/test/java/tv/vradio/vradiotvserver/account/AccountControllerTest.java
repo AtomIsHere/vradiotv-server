4965aeb5052f2d02eef063c50da67008d91fa554
AtomIsHere <archietomoconnor@gmail.com>
2022-08-27 17:02:39 +1000

    Add account tests

---
+++ src/test/java/tv/vradio/vradiotvserver/account/AccountControllerTest.java
@@ -0,0 +1,80 @@
+package tv.vradio.vradiotvserver.account;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import tv.vradio.vradiotvserver.RedisExtension;
+import tv.vradio.vradiotvserver.account.auth.AuthRepository;
+import tv.vradio.vradiotvserver.account.auth.AuthToken;
+import tv.vradio.vradiotvserver.exceptions.AuthenticationFailureException;
+
+import java.util.UUID;
+
+@SpringBootTest
+@ExtendWith(RedisExtension.class)
+@DirtiesContext
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class AccountControllerTest {
+    @Autowired
+    private AccountController controller;
+
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private AuthRepository authRepository;
+
+    @Test
+    @Order(1)
+    public void createAccountTest() {
+        // Create account and check if it's successful
+        assertEquals(controller.createAccount(TestAccountData.USERNAME, TestAccountData.EMAIL, TestAccountData.PASSWORD), AccountController.CreationResult.SUCCESS);
+
+        // Check if it is present in the repository
+        assertTrue(accountRepository.existsByUsername(TestAccountData.USERNAME));
+        assertTrue(accountRepository.existsByEmail(TestAccountData.EMAIL));
+    }
+
+    @Test
+    @Order(2)
+    public void checkAuthTest() {
+        // Create token
+        AuthToken token = controller.login(TestAccountData.USERNAME, TestAccountData.PASSWORD);
+
+        // Check if token is valid
+        assertTrue(controller.checkAuth(TestAccountData.USERNAME, token.id().toString()));
+        // Check if invalid token throws an exception
+        assertThrows(AuthenticationFailureException.class, () -> controller.checkAuth(TestAccountData.USERNAME, "a"));
+        // Check if incorrect token is detected as such
+        assertFalse(controller.checkAuth(TestAccountData.USERNAME, UUID.randomUUID().toString()));
+
+        authRepository.deleteAll();
+    }
+
+    @Test
+    @Order(2)
+    public void repeatedAuthTest() {
+        AuthToken oldToken = controller.login(TestAccountData.USERNAME, TestAccountData.PASSWORD);
+        AuthToken newToken = controller.login(TestAccountData.USERNAME, TestAccountData.PASSWORD);
+
+        assertNotEquals(oldToken.id().toString(), newToken.id().toString());
+
+        assertTrue(controller.checkAuth(TestAccountData.USERNAME, newToken.id().toString()));
+        assertFalse(controller.checkAuth(TestAccountData.PASSWORD, oldToken.id().toString()));
+
+        authRepository.deleteAll();
+    }
+
+    @Test
+    @Order(2)
+    public void logoutTest() {
+        AuthToken token = controller.login(TestAccountData.USERNAME, TestAccountData.PASSWORD);
+
+        assertTrue(controller.checkAuth(TestAccountData.USERNAME, token.id().toString()));
+        assertTrue(controller.logout(token.id().toString()));
+        assertFalse(controller.checkAuth(TestAccountData.USERNAME, token.id().toString()));
+    }
+}
---
+++ src/test/java/tv/vradio/vradiotvserver/account/TestAccountData.java
@@ -0,0 +1,7 @@
+package tv.vradio.vradiotvserver.account;
+
+public class TestAccountData {
+    public static final String USERNAME = "testAccount";
+    public static final String EMAIL = "test@test.com";
+    public static final String PASSWORD = "testPassword";
+}
